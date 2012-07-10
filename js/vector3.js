acfb37a385ba1ef69b78bec7dba2fba08fe71fad
Denis Narushevich <3859032+denisnarushevich@users.noreply.github.com>
2012-07-10 23:29:32 +0300

    vect

---
+++ js/vector3.js
@@ -0,0 +1,16 @@
+define(['vector'], function(vector){
+  var vector3 = Object.create(vector);
+  
+  vector3.z = null;
+  
+  vector3.getZ = function(){
+    return this.z;
+  }
+  
+  vector3.setZ = function(z){
+    this.z = z;
+    return this;
+  }
+});
+
+
