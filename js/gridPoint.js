df991a9a9051b1b7cda805cf99e5357ad07111bb
D Narushevich <denis@narushevich.lv>
2012-07-14 03:53:07 +0300

    post tile structural remake

--- js/gridPoint.js
+++ js/gridPoint.js
@@ -1,16 +1,19 @@
 define(['vector3', 'simplex'], function(point, Simplex){
   var gridPoint = Object.create(point);
-    
+  
+  gridPoint.z0 = null; //pure grid point value, without added waterLevel
+  gridPoint.w = null; //water height\depth on given tile
+  
   //get point of grid, with set height. slopes of terrain shouldnt exceed 45 degrees.
   //This realisation is using many octaves, to have realistic view of terrain contour, coastline.
   //but because at the same time we need it to have smooth slopec <45Deg, there's a drawback - its too flat
   //height varies only from -16 to 16. can't afford anymore, because then there will be cliffs.
   //it would be good to invent something better then this aproach, with smooth slopes  and different landscape
-  gridPoint.getZ = function(){
+  gridPoint.getZ0 = function(){
     var x = this.getX();
     var y = this.getY();
 
-    if ( typeof(this.z) == 'number' ) return this.z;
+    if ( typeof(this.z0) == 'number' ) return this.z0;
     else if ( x % 1  || y % 1 ) {
       var grid = g.logic.world.grid;
       var x0 = Math.floor(x), y0 = Math.floor(y);
@@ -34,11 +37,9 @@ define(['vector3', 'simplex'], function(point, Simplex){
       }
       
       if(y<0)
-        this.z = i2 * (1-yf) + i1 * yf; 
+        this.z0 = i2 * (1-yf) + i1 * yf; 
       else
-        this.z = i1 * (1-yf) + i2 * yf; 
-       
-      return this.z;
+        this.z0 = i1 * (1-yf) + i2 * yf; 
     } else {
       var land = 0, island = 0;
 
@@ -55,9 +56,27 @@ define(['vector3', 'simplex'], function(point, Simplex){
       island += Simplex.noise2d(x / 16, y / 16) / 40;
       island += Simplex.noise2d(x / 8, y / 8) / 40;
 
-      return this.z = Math.floor( (0.8*land + 0.2*island) * 16 );
-    }    
+      this.z0 = Math.floor( (0.8*land + 0.2*island) * 16 );
+    }
+    
+    return this.z0;
   };
+  
+  //water depth on given point
+  gridPoint.getW = function(){
+    if ( typeof(this.w) == 'number' ) return this.w;
+    
+    var z0 = this.getZ0();
+    return this.w = z0 <= g.logic.world.waterLevel ? g.logic.world.waterLevel - z0 + 1 : 0;
+  }
+
+  //grid point Z value, with added water level.
+  gridPoint.getZ = function(){
+    if ( typeof(this.z) == 'number' ) return this.z;
+    
+    var z0 = this.getZ0();
+    return this.z = z0 <= g.logic.world.waterLevel ? g.logic.world.waterLevel : z0;
+  }
 
   return gridPoint;
 });
